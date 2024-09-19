bf76319e63dd94f96d4d77ff866c38e29888548c
kishore <kishore.d21@iiits.in>
2024-09-19 21:47:55 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TextEditor from './components/TextEditor.jsx'
+import './App.css';
+
+const App = () => {
+  return (
+    <div><TextEditor/></div>
+  )
+}
+
+export default App
